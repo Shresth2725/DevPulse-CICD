854dda633c6f7f78a42bc8af0cc7008f3b0e032c
Shresth <2725shresth@gmail.com>
2026-01-22 23:54:17 +0530

    bug fix

--- terraform/jenkins/main.tf
+++ terraform/jenkins/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "jenkins_ec2" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t3.micro"
+  instance_type          = "t3.small"
   vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
   key_name               = "DevPulse-Devops-Key"
 
